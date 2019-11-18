7eaedf293397aa90edf0d1d268352d34e46ec747
Agathe Badia <ab@honeypot.io>
2019-11-18 17:17:49 +0100

    Controller, route

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,13 @@
 class BookingsController < ApplicationController
   # We need to display all the bookings done on the tool details' page (INDEX)
+
+  # view will be on the show tool page
+
+  # Actions of the controller
+  # Routes
+  # Views
+
+  def index
+    @bookings.tool = @tool
+  end
 end
