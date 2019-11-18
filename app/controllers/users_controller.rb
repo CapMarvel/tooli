e05da7ce77c8ecc5ce780cfa7291f306bcde321e
Agathe Badia <ab@honeypot.io>
2019-11-18 16:26:56 +0100

    Pseudo code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+  # Authentication / Devise for sign in, sign up
+  # Show the profile of the user (password, address, email)
+  # All routes, except index (because we do not need to display all users)
 end
