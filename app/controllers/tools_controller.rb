6e56822bf5fd7897ae23fe75578fcc713ff68550
Leonardo Costa <leo.costa.programming@gmail.com>
2019-11-18 17:45:24 +0100

    tools_controller

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,4 +1,22 @@
 class ToolsController < ApplicationController
-  # We need the 7 general routes for Tools (index, show, create, new, edit, update, delete)
+  def index
+  end
 
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- test/controllers/tools_controller_test.rb
+++ test/controllers/tools_controller_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class ToolsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get tools_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get tools_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get tools_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get tools_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get tools_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get tools_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get tools_destroy_url
+    assert_response :success
+  end
+
 end
