412ab7264d10ad5a30d37ba397bce682565ef242
Agathe Badia <ab@honeypot.io>
2019-11-19 12:50:54 +0100

    As a user, I can add a tool, IF I'm registered

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -11,6 +11,7 @@ class ToolsController < ApplicationController
 
   def create
     @tool = Tool.new(tool_params)
+    @tool.user_id = current_user.id
     if @tool.save
       redirect_to @tool
     else
