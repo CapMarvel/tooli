065bb26869012e179ecb87a4619f971fd0bd562e
Agathe Badia <ab@honeypot.io>
2019-11-21 16:18:07 +0100

    solved issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,8 @@ class UsersController < ApplicationController
   # All routes, except index (because we do not need to display all users)
 
 
-
   def show
     @user_tools = current_user.tools
     @user_bookings = Booking.where(user_id: "#{current_user.id}")
-    raise
   end
 end
